6514a7226a1aad67b4e07ec3c32c56f4c6e2e300
Aotumuri <Aotumuri.YT@gmail.com>
2026-01-05 20:34:09 +0900

    refactor: store bot info as constants

---
+++ src/info/constants.py
@@ -0,0 +1,4 @@
+BOT_NAME = "OpenFrontStatus"
+BOT_AUTHOR = "Aotumuri <@1198183481545592893>"
+BOT_GIT_URL = "https://github.com/aotumuri/openfront-game-bot"
+BOT_CONTRIBUTORS_RAW = "raimakanisuto <@1401109328014082088>"
--- src/info/service.py
+++ src/info/service.py
@@ -2,18 +2,20 @@ from typing import Optional, Tuple
 
 import discord
 
-from info.api import get_bot_author, get_bot_name, get_contributors, get_git_url
+from info.constants import (
+    BOT_AUTHOR,
+    BOT_CONTRIBUTORS_RAW,
+    BOT_GIT_URL,
+    BOT_NAME,
+)
 from info.embeds import build_info_embed
 
 
 def build_info_payload() -> Tuple[Optional[discord.Embed], Optional[str]]:
-    try:
-        bot_name = get_bot_name()
-        author = get_bot_author()
-        git_url = get_git_url()
-        contributors = get_contributors()
-    except RuntimeError as exc:
-        return None, str(exc)
-
-    embed = build_info_embed(bot_name, author, git_url, contributors)
+    contributors = [
+        item.strip()
+        for item in BOT_CONTRIBUTORS_RAW.split(",")
+        if item.strip()
+    ]
+    embed = build_info_embed(BOT_NAME, BOT_AUTHOR, BOT_GIT_URL, contributors)
     return embed, None
